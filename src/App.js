f572716ca4cf4f94080a841512c4591a0b8c0c96
Adlington <MA4135@zebra.com>
2022-10-26 11:17:27 +0100

    reverted app to template

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { StripesTheme, Button } from '@zebra-fed/zds-react';
 
 function App() {
   return (
-    <StripesTheme>
-      <Button type={'primary'}>Hello</Button>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </StripesTheme>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
